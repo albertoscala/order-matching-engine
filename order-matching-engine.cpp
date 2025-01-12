7602d1383ec62759fbf425b1e545881135fd4326
Alberto Scala <alby.scala@gmail.com>
2025-01-12 01:55:02 +0100

    messages done

--- order-matching-engine.cpp
+++ order-matching-engine.cpp
@@ -1,29 +1,33 @@
 #include<iostream>
 #include<optional>
+#include<sstream>
+#include<vector>
 using namespace std;
 
+vector<string> split(string s, char delimiter);
+
 enum class Side {
-    BUY,
-    SELL
+    BUY = 1,
+    SELL = 2
 };
 
 enum class OrderType {
-    MARKET,
-    LIMIT,
-    STOP,
-    STOP_LIMIT
+    MARKET = 1,
+    LIMIT = 2,
+    STOP = 3,
+    STOP_LIMIT = 4
 };
 
 class Message {
 private:
-    optional<int> orderId;      // Unique identifier for the order
-    optional<int> clientId;     // Client placing the order
-    optional<string> instrument;   // Financial instrument
-    optional<int> side;         // Buy or sell
-    optional<int> quantity;     // NUmber of units to trade
-    optional<int> type;         // Order type
-    optional<int> price;        // Price for the limit or stop-limit orders
-    optional<int> stopPrice;    // Stop price for the stop and stop-limit orders
+    optional<int> orderId;          // Unique identifier for the order
+    optional<int> clientId;         // Client placing the order
+    optional<string> instrument;    // Financial instrument
+    optional<int> side;             // Buy or sell
+    optional<int> quantity;         // Number of units to trade
+    optional<int> type;             // Order type
+    optional<int> price;            // Price for the limit or stop-limit orders
+    optional<int> stopPrice;        // Stop price for the stop and stop-limit orders
 public:
     Message(optional<int> orderId, optional<int> clientId, optional<string> instrument, optional<int> side, optional<int> quantity, optional<int> type, optional<int> price, optional<int> stopPrice) {
         if (orderId.has_value()) {
@@ -49,6 +53,37 @@ public:
         }
         if (stopPrice.has_value()) {
             this->stopPrice = stopPrice;
+        }
+    };
+
+    Message(string fixMessage) {
+        vector<string> fields = split(fixMessage, '|');
+
+        for (string field : fields) {
+            vector<string> keyValue = split(field, '=');
+
+            // Awesome error handling
+            if (keyValue.size() != 2) {
+                continue;
+            }
+
+            if (keyValue[0] == "11") {
+                this->orderId = stoi(keyValue[1]);
+            } else if (keyValue[0] == "1") {
+                this->clientId = stoi(keyValue[1]);
+            } else if (keyValue[0] == "55") {
+                this->instrument = keyValue[1];
+            } else if (keyValue[0] == "54") {
+                this->side = stoi(keyValue[1]);
+            } else if (keyValue[0] == "38") {
+                this->quantity = stoi(keyValue[1]);
+            } else if (keyValue[0] == "40") {
+                this->type = stoi(keyValue[1]);
+            } else if (keyValue[0] == "44") {
+                this->price = stoi(keyValue[1]);
+            } else if (keyValue[0] == "99") {
+                this->stopPrice = stoi(keyValue[1]);
+            }
         }
     };
 
@@ -87,9 +122,48 @@ public:
 
     };
 
-    string toFixMessage() {};
+    string toFixMessage() {
+        string fixMessage = "";
 
-    void unpackFixMessage(string fixMessage) {};
+        if (orderId.has_value()) {
+            fixMessage += "11=" + to_string(orderId.value()) + "|";
+        }
+
+        if (clientId.has_value()) {
+            fixMessage += "1=" + to_string(clientId.value()) + "|";
+        }
+
+        if (instrument.has_value()) {
+            fixMessage += "55=" + instrument.value() + "|";
+        }
+
+        if (side.has_value()) {
+            fixMessage += "54=" + to_string(side.value()) + "|";
+        }
+
+        if (quantity.has_value()) {
+            fixMessage += "38=" + to_string(quantity.value()) + "|";
+        }
+
+        if (type.has_value()) {
+            fixMessage += "40=" + to_string(type.value()) + "|";
+        }
+
+        if (price.has_value()) {
+            fixMessage += "44=" + to_string(price.value()) + "|";
+        }
+
+        if (stopPrice.has_value()) {
+            fixMessage += "99=" + to_string(stopPrice.value()) + "|";
+        }
+
+        return fixMessage;
+    };
+
+    void unpackFixMessage(string fixMessage) {
+        
+
+    };
 };
 
 class MessageBuilder {
@@ -153,5 +227,26 @@ int main() {
 
     m.display();
 
+    cout << m.toFixMessage() << endl;
+
+    Message m2 = Message(m.toFixMessage());
+
+    m2.display();
+
+    cout << m2.toFixMessage() << endl;
+
     return 0;
+}
+
+vector<string> split(string s, char delimiter) {
+    vector<string> fields;
+    stringstream ss(s);
+    string field;
+
+    // Split by delimiter
+    while (getline(ss, field, delimiter)) {
+        fields.push_back(field);
+    }
+
+    return fields;
 }
