8643c3deab37b57166db799088cfa5bee084b6c6
Alberto Scala <31989626+albertoscala@users.noreply.github.com>
2025-01-15 13:53:32 +0100

    main implementation finished

--- order-matching-engine.cpp
+++ order-matching-engine.cpp
@@ -57,6 +57,48 @@ int checkSellLiquidity() {
     return liquidity;
 }
 
+int checkBuyLiquidityFromPrice(float price) {
+    int liquidity = 0;
+
+    // Check if the first price level greater or equal to the given price exists
+    if (buyOrders.lower_bound(price) != buyOrders.end()) {
+        
+        // Iterate over the buy orders
+        for (auto it = buyOrders.lower_bound(price); it != buyOrders.end(); it++) {
+            
+            // Iterate over the orders at the current price level
+            for (auto it2 = it->second.begin(); it2 != it->second.end(); it2++) {
+                
+                // Add the quantity of the order to the total liquidity
+                liquidity += Message(*it2).getQuantity();
+            }
+        }
+    }
+
+    return liquidity;
+}
+
+int checkSellLiquidityFromPrice(float price) {
+    int liquidity = 0;
+
+    // Check if the first price level greater or equal to the given price exists
+    if (sellOrders.lower_bound(price) != sellOrders.end()) {
+        
+        // Iterate over the buy orders
+        for (auto it = sellOrders.lower_bound(price); it != sellOrders.end(); it++) {
+            
+            // Iterate over the orders at the current price level
+            for (auto it2 = it->second.begin(); it2 != it->second.end(); it2++) {
+                
+                // Add the quantity of the order to the total liquidity
+                liquidity += Message(*it2).getQuantity();
+            }
+        }
+    }
+
+    return liquidity;
+}
+
 // Market Order Flows
 
 // NO FUCKING CLUE I GUESS IT'S CORRECT IDK WHY BUT I THINK IT IS
@@ -174,9 +216,125 @@ void marketSellFlow(Message m) {
 
 }
 
+// Limit Order Flows
+
+// Limit Buy Order Flow
+void limitBuyFlow(Message m) {
+    // Check if there is enough liquidity to fill the order
+    if (checkSellLiquidityFromPrice(m.getPrice().value()) >= m.getQuantity()) {
+        
+        // Iterate over the sell orders
+        for (auto it = sellOrders.lower_bound(m.getPrice().value()); it != sellOrders.end(); it++) {
+            
+            // Iterate over the orders at the current price level
+            for (auto it2 = it->second.begin(); it2 != it->second.end(); it2++) {
+                
+                // Get the order
+                Message order = Message(*it2);
+
+                // Check if the order quantity is greater than the limit order quantity
+                if (order.getQuantity() >= m.getQuantity()) {
+                    
+                    // Update the order quantity
+                    order.setQuantity(order.getQuantity() - m.getQuantity());
+
+                    // Check if the order quantity is zero
+                    if (order.getQuantity() == 0) {
+                        // Remove the order
+                        sellOrders[it->first].erase(it2);
+                    }
+
+                    // Update the order inposition
+                    sellOrders[it->first].insert(it2, order.toFixMessage());
+
+                    // Record the trade
+                    trades.push_back(Transaction(trades.size() + 1, m.getOrderId(), m.getClientId(), order.getClientId(), m.getInstrument(), m.getQuantity(), order.getPrice().value()));
+
+                    // Break the loop
+                    break;
+                } else {
+                    
+                    // Update the limit order quantity
+                    m.setQuantity(m.getQuantity() - order.getQuantity());
+
+                    // Remove the order
+                    sellOrders[it->first].erase(it2);
+                }
+            }
+
+            // Check if the limit order quantity is zero
+            if (m.getQuantity() == 0) {
+                break;
+            }
+        }
+
+    } else {
+        // Not enough liquidity to fill the order so add it to the order book
+        buyOrders[m.getPrice().value()].push_back(m.toFixMessage());
+    }
+
+}
+
+// Limit Sell Order Flow
+void limitSellFlow(Message m) {
+    // Check if there is enough liquidity to fill the order
+    if (checkBuyLiquidityFromPrice(m.getPrice().value()) >= m.getQuantity()) {
+        
+        // Iterate over the buy orders
+        for (auto it = buyOrders.lower_bound(m.getPrice().value()); it != buyOrders.end(); it++) {
+            
+            // Iterate over the orders at the current price level
+            for (auto it2 = it->second.begin(); it2 != it->second.end(); it2++) {
+                
+                // Get the order
+                Message order = Message(*it2);
+
+                // Check if the order quantity is greater than the limit order quantity
+                if (order.getQuantity() >= m.getQuantity()) {
+                    
+                    // Update the order quantity
+                    order.setQuantity(order.getQuantity() - m.getQuantity());
+
+                    // Check if the order quantity is zero
+                    if (order.getQuantity() == 0) {
+                        // Remove the order
+                        buyOrders[it->first].erase(it2);
+                    }
+
+                    // Update the order inposition
+                    buyOrders[it->first].insert(it2, order.toFixMessage());
+
+                    // Record the trade
+                    trades.push_back(Transaction(trades.size() + 1, m.getOrderId(), m.getClientId(), order.getClientId(), m.getInstrument(), m.getQuantity(), order.getPrice().value()));
+
+                    // Break the loop
+                    break;
+                } else {
+                    
+                    // Update the limit order quantity
+                    m.setQuantity(m.getQuantity() - order.getQuantity());
+
+                    // Remove the order
+                    buyOrders[it->first].erase(it2);
+                }
+            }
+
+            // Check if the limit order quantity is zero
+            if (m.getQuantity() == 0) {
+                break;
+            }
+        }
+
+    } else {
+        // Not enough liquidity to fill the order so add it to the order book
+        sellOrders[m.getPrice().value()].push_back(m.toFixMessage());
+    }
+}
+
 // Order Matching Engine
 // Time Priority
-// Market order with no liquidity are rejected
+// Fill or Kill
+
 void addOrder(string s) {
     Message m = Message(s);
 
